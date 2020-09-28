01c2ec9e74312c77768357290c9d222236a9ec45
LysakArtem <Perry.dev7@gmail.com>
2020-09-28 22:24:48 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  
+const bodyRef= document.querySelector('body');
+const buttonStartRef= document.querySelector('[data-action="start"]');
+const buttonStopRef= document.querySelector('[data-action="stop"]');
+
+buttonStartRef.addEventListener('click', onClickStart);
+buttonStopRef.addEventListener('click', onClickStop);
+
+const changeBackground=()=>{
+    const randomIndex=randomIntegerFromInterval(0, 5);
+    const color= colors[randomIndex];
+    let value= `background-color: ${color}`;
+    bodyRef.setAttribute('style',value);
+    console.log(color);
+}
+let intervalId=null
+function onClickStart(){ 
+ intervalId= setInterval(changeBackground,1000);
+ buttonStartRef.disabled = true;
+}
+
+function onClickStop(){
+    if(intervalId){
+        clearInterval(intervalId);
+        buttonStartRef.disabled =false;
+    }
+}
+
+
